2742a5a429fed16fadf8361bbbe6e53d3bb328a6
jessicavitoriatavares <102222297+jessicavitoriatavares@users.noreply.github.com>
2022-04-25 21:31:21 -0300

    Classe Conta

---
+++ Conta.cs
@@ -0,0 +1,48 @@
+public class Conta
+{
+    public int Codigo { get; }
+    public double Saldo { get; private set;}
+
+    public Conta(int codigo)
+    {
+        Codigo = codigo;
+        Saldo = 0.0;
+    }
+
+    public void Depositar(double valor)
+    {
+        ValidarValor(valor);
+        VerificarSaldo(valor);
+        Saldo = Saldo + valor;
+    }
+
+    public void Sacar(double valor)
+    {
+        ValidarValor(valor);
+        Saldo = Saldo - valor;
+    } 
+
+
+    public void Transferir(double valor, Conta conta)
+    {
+        //VerificarSaldo(valor);
+        //ValidarValor(valor);
+        //Saldo = Saldo - valor;
+        //conta.Saldo = conta.Saldo + valor;
+
+        Sacar(valor);
+        conta.Depositar(valor);
+    }
+
+    private void ValidarValor(double valor)
+    {
+        if(valor <= 0.0)
+               throw new ArgumentException("valor deve ser maior que zero");
+    }
+
+    private void VerificarSaldo(double valor)
+    {
+        if(valor > Saldo)
+               throw new ArgumentException("Saldo insuficiente");
+    }
+} 
